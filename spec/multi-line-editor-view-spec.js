10bbdf764af310becba195fb0f68dfa662324e91
gradrix <grazjuc@gmail.com>
2017-10-19 00:59:25 +0300

    Project commit

---
+++ spec/multi-line-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MultiLineEditorView from '../lib/multi-line-editor-view';
+
+describe('MultiLineEditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
